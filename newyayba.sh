6cfc7c5642cd2d638c3eebf1318c1d3fd35a2a84
daniel.abbott <nannal@techious.com>
2016-02-04 17:17:36 +0000

    How much fun is this eh?

---
+++ newyayba.sh
@@ -0,0 +1,1 @@
+echo $(curl -Ss https://www.reddit.com/r/listentothis/new|sed s/'<'/\\n/g|sed s/'href='/\\n/g|grep -i Youtube|cut -f 2 -d '"'|grep -i Youtube|grep -v domain|grep -v index|grep -v attribution|sort -n|sed -n '1~2!p'|cut -f 2 -d '=')|sed s/' '/,/g|sed s/^/'www.youtube.com\/watch_videos?video_ids='/
